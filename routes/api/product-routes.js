55aa2dd30202afd415d56ba1095a3bd35b3fefe7
Zonia Ramirez <jslnramirez@gmail.com>
2024-08-03 11:50:20 -0400

    updating product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,43 @@ const { Product, Category, Tag, ProductTag } = require("../../models");
 // The `/api/products` endpoint
 
 // get all products
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const products = await Product.findAll({
+      include: [
+        { model: Category },
+        { model: Tag, through: ProductTag }
+      ]
+    });
+    res.status(200).json(products);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+    try {
+      const product = await Product.findByPk(req.params.id, {
+        include: [
+          { model: Category },
+          { model: Tag, through: ProductTag }
+        ]
+      });
+      if (!product) {
+        res.status(404).json({ message: 'No product found with this id!' });
+        return;
+      }
+      res.status(200).json(product);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
 });
 
 // create new product
@@ -70,8 +98,23 @@ router.put("/:id", async (req, res) => {
   }
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
